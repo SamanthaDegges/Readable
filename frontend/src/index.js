3e36e7231d81507254663553da7228f7e48c02f4
Samantha Degges <samanthadegges@gmail.com>
2017-10-09 15:34:25 -0700

    add redux, react-redux, provider,and log middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux';
 
- //create store here
+//store returns a function that calls .next(action(){} and logs a bunch of crap before it actually dispatches the action.
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
